590d3b05428047ad7d0d6e7ff6cd5dba4612e1f7
Sushant Sahu <sushant87@gmail.com>
2020-04-05 16:48:04 +0530

    fix: minor changes in message

--- packages/hello-world-webapp/src/App.js
+++ packages/hello-world-webapp/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Sushant & Khusboo
+          Learn React.js with Sushant & Khusboo
         </a>
       </header>
     </div>
